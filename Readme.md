6fc013384fc57be9aa9260df5d7a7b4b58588d4f
abominavelneves <pythonproject911@gmail.com>
2024-07-20 22:36:46 -0300

    Fixing the code

--- Readme.md
+++ Readme.md
@@ -9,3 +9,4 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains as AC
 ```
+#### Objectives
--- sigaa.py
+++ sigaa.py
@@ -5,12 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains as AC
 
 ##Login Informations
-username='222025100'
-password='Neves2906'
+print('Username: ')
+username=input()
+print('Password: ')
+password=input()
+
 
 ##Using Chrome
 driver=webdriver.Chrome()
 driver.get('https://autenticacao.unb.br/sso-server/login?service=https://sig.unb.br/sigaa/login/cas')
+driver.maximize_window()
 try:
     initial=WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,"username")))
     driver.find_element(By.ID,"username").send_keys(username)
@@ -18,7 +22,8 @@ try:
     driver.find_element(By.NAME,"submit").click()
     second=WebDriverWait(driver,50).until(EC.presence_of_element_located((By.ID,"conteudo")))
     ensino=driver.find_element(By.CLASS_NAME,"ThemeOfficeMainItem")
-    AC(driver).move_to_element(ensino).click().perform()
+    AC(driver).move_to_element(ensino).click().move_to_element_with_offset(ensino,0,250).click().move_by_offset(390,0).click().perform()
     
 finally:
+    WebDriverWait(driver,10)
     driver.quit()
